1eb540dd9649eb2605822e8d51e9ff90e1a05574
Chike Ezeh <ezeh.chike@gmail.com>
2017-08-26 15:08:06 -0500

    adds the harmlessRansomeNote algorithm

--- 2_harmlessRansomeNote/harmlessRansomeNote.py
+++ 2_harmlessRansomeNote/harmlessRansomeNote.py
@@ -1,13 +1,31 @@
-noteText = 'This is a secret note for you from a secret admirer'
+noteText = 'this is a note'
 
-magazineText ='''puerto rico is a great place you must hike far from town to find
-a secret waterfall that I am an admirer of but note that it is not as hard as it
-seems this is my advice for you'''
+magazineText ='take note this is all the magazine text in a magazine'
+
 def harmlessRansomeNote(a,b):
-    if len(a) > len(b):
-        return a
-        
+    noteArr = a.split(' ') # make a list out of the strings in noteText
+    magazineArr = b.split(' ') # make a list out of the strings in magazineText
+    # create a dictionary using magazineArr, the key is the number of times
+    # the value in the list appears.
+    magazineDict ={}
+    for i in magazineArr:
+        if i not in magazineDict:
+            magazineDict[i] = 0
+        magazineDict[i] += 1
 
+    # bolean to keep track of if we have enough words to make the ransom note.
+    noteIsPossible = True
+    # now we loop through the noteArr, and check if each word is in our magazineDict
+    # as we loop through, we decrease the value of each word in the magazineDict
+
+    for i in noteArr:
+        if i in magazineDict:
+            magazineDict[i] -=1
+            if magazineDict[i] < 0:
+                noteIsPossible = False
+        else:
+            noteIsPossible = False
+    return noteIsPossible
 
 if __name__ == '__main__': # allows the program to be run by calling the file name
-    harmlessRansomeNote(noteText,magazineText)
+    print(harmlessRansomeNote(noteText,magazineText))
