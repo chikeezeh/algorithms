5ce3ef06e707c54d9efe5b07be6b22b454d4f9fa
Chike Ezeh <cezeh@tulane.edu>
2023-08-23 18:30:51 -0700

    quick sort

---
+++ quicksort.py
@@ -0,0 +1,19 @@
+def quicksort(arr):
+    if len(arr) <=1:
+        return arr
+    pivot = arr[-1]
+    left = []
+    right = []
+    
+    return arr
+
+
+a = [3,8,5,1,10]
+b = [1,2,3,4,5,6]
+c = []
+d = [6,5,4,3,2,1]
+
+print(quicksort(a))
+print(quicksort(b))
+print(quicksort(c))
+print(quicksort(d))
