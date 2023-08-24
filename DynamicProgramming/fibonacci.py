880df4526c2c66ff39ac9e41de0f6de5d83b3c1a
Chike Ezeh <cezeh@tulane.edu>
2023-08-23 20:20:02 -0700

    using memoization for fibonacci.

--- DynamicProgramming/fibonacci.py
+++ DynamicProgramming/fibonacci.py
@@ -1,9 +1,12 @@
-def fib_rec(n):
-    # naive solution
+def fib_rec(n, memo={}):
+    # optimized solution
+    if n in memo:
+        return memo[n]
     if n <=2:
         return 1
     else:
-        return (fib_rec(n-1) + fib_rec(n-2))
+        memo[n] = (fib_rec(n-1, memo) + fib_rec(n-2, memo))
+        return memo[n]
 
 # def fib_rec(n):
 #     # naive solution
@@ -14,6 +17,10 @@ def fib_rec(n):
 
 
 print(fib_rec(1))
+print(fib_rec(2))
 print(fib_rec(3))
+print(fib_rec(4))
 print(fib_rec(5))
-print(fib_rec(100))
+print(fib_rec(6))
+print(fib_rec(7))
+print(fib_rec(50))
