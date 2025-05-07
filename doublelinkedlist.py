6fb3db6ddf4916ea86ce719bfc12ab886680c9b4
Chike Ezeh <cezeh@tulane.edu>
2025-05-06 19:33:37 -0700

    fixed the logic error bug in the print_list method, initially this will throw a name error if  a doubly linked list was empty.

--- doublelinkedlist.py
+++ doublelinkedlist.py
@@ -16,7 +16,17 @@ class DoublyLinkedList:
     
     def append(self, data):
         # add a node to the end of a dll
-        pass
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        cur_node = self.head
+        while cur_node.next:
+            cur_node = cur_node.next
+        cur_node.next = new_node
+        new_node.prev = cur_node
+
+
 
     def prepend(self,data):
         # add a node to the beginning of a dll
@@ -24,10 +34,17 @@ class DoublyLinkedList:
 
     def print_list(self):
         # print the nodes of a dll
-        if self.head:
-            result = []
-            cur_node = self.head
-            while cur_node:
-                result.append(cur_node.data)
-        print("null <--"+" <--> ".join(result) + " --> null")
+        result = []
+        cur_node = self.head
+        while cur_node:
+            result.append(cur_node.data)
+            cur_node = cur_node.next
+        print("null <-- "+" <--> ".join(result) + " --> null")
+
+dll1 = DoublyLinkedList()
+dll1.print_list()
+dll1.append("A")
+dll1.print_list()
+dll1.append("B")
+dll1.print_list()
 
