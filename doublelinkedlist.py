0ef7e764229f2035dddd844e37fa2d394e6f75fc
Chike Ezeh <cezeh@tulane.edu>
2025-05-06 19:53:32 -0700

    implemented a method for adding a node after a node.

--- doublelinkedlist.py
+++ doublelinkedlist.py
@@ -46,6 +46,22 @@ class DoublyLinkedList:
             result.append(str(cur_node.data))
             cur_node = cur_node.next
         print("null <-- "+" <--> ".join(result) + " --> null")
+    def add_after_node(self,key,data):
+        # add a node to a dll after a node.data = key
+        cur_node = self.head
+        while cur_node:
+            if not cur_node.next and cur_node.data == key:
+                self.append(data)
+                return
+            elif cur_node.data == key:
+                new_node = Node(data)
+                nxt = cur_node.next #keep track on the next of current node before we swapp pointers.
+                cur_node.next = new_node
+                new_node.next = nxt
+                new_node.prev = cur_node
+                nxt.prev = new_node
+                return
+            cur_node = cur_node.next
 
 dll1 = DoublyLinkedList()
 dll1.prepend("F")
@@ -56,5 +72,8 @@ dll1.append("B")
 dll1.print_list()
 dll1.append("C")
 dll1.prepend(50)
+dll1.print_list()
+
+dll1.add_after_node("F","X")
 dll1.print_list()
 
