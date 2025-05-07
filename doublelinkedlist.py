68ec2d1698373fe2d928b07a5d1a47a1301e3ace
Chike Ezeh <cezeh@tulane.edu>
2025-05-06 19:09:15 -0700

    created a method to print out the nodes of a doubly linked list, and also created the structure of the append and prepend methods.

--- doublelinkedlist.py
+++ doublelinkedlist.py
@@ -10,3 +10,24 @@ class Node:
         self.next = None
         self.prev = None
 
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        # add a node to the end of a dll
+        pass
+
+    def prepend(self,data):
+        # add a node to the beginning of a dll
+        pass
+
+    def print_list(self):
+        # print the nodes of a dll
+        if self.head:
+            result = []
+            cur_node = self.head
+            while cur_node:
+                result.append(cur_node.data)
+        print("null <--"+" <--> ".join(result) + " --> null")
+
