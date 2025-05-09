166a79f9530e6d8b4bf3a00fa856c525869afbde
Chike Ezeh <cezeh@tulane.edu>
2025-05-08 19:56:42 -0700

    created a version that accepts input from the cli

--- mostoccurence.py
+++ mostoccurence.py
@@ -1,5 +1,6 @@
 from re import findall
 from collections import Counter
+from sys import argv
 """
 read in a file , return the most occuring word in the file.
 constraints:
@@ -31,5 +32,18 @@ def top_words_counter(filepath, n=1):
     word_counts = Counter(contents)
     return word_counts.most_common(n)
 
-print(top_words('poem.txt',5))
-print(top_words_counter('poem.txt',5))
+def top_words_cli(filepath, n=1):
+    with open(filepath, 'r',encoding='utf-8') as text_file:
+        contents = text_file.read().lower()
+    contents = findall(r'\b[a-z]+\b', contents)
+    word_counts = Counter(contents)
+    return word_counts.most_common(n)
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        print(f"Usage: python {argv[0]} <filename>")
+    else:
+        print(top_words_cli(argv[1], int(argv[2])))
+
+# print(top_words('poem.txt',5))
+# print(top_words_counter('poem.txt',5))
