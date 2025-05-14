394f4b30f6a4d21171208299be19b7ec687161b4
Chike Ezeh <cezeh@tulane.edu>
2025-05-13 20:18:49 -0700

    Used a hasmap to reduce the time complexity, but the tradeoff is use more space.

--- twosum.py
+++ twosum.py
@@ -14,5 +14,18 @@ def two_sum_brute(A, target):
             if A[i] + A[j] == target:
                 return (A[i], A[j])
     return "No pair"
+def two_sum_hash(A, target):
+    # this uses a hashmap to store the elements as we 
+    # traverse the array
+    # O(n) time and 0(n) space
+    target_hash = {}
+    for element in A:
+        if element in target_hash:
+            return (target_hash[element], element)
+        else:
+            target_hash[target - element] = element
+    return "No pair"
 
-print(two_sum_brute([2,3,4,5,7], 14))
+
+print(two_sum_brute([2,3,4,5,7], 11))
+print(two_sum_hash([2,3,4,5,7], 11))
