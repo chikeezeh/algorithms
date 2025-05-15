45c615c5f6f1aa4691ff3c76bab1bb26cbe91670
Chike Ezeh <cezeh@tulane.edu>
2025-05-14 20:01:40 -0700

    Created a method for post order traversal

--- binarytree.py
+++ binarytree.py
@@ -20,6 +20,13 @@ class BinaryTree(object):
             traversal += str(start.value) + "->"
             traversal = self.inorder_print(start.right, traversal)
         return traversal
+    def post_order_print(self, start, traversal):
+        """Left->Right->Root"""
+        if start:
+            traversal = self.pre_order_print(start.left, traversal)
+            traversal = self.pre_order_print(start.right, traversal)
+            traversal += str(start.value) + "->"
+        return traversal
     
 
 
@@ -30,5 +37,17 @@ tree.root.left.left = Node(4)
 tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
-print(tree.pre_order_print(tree.root, ""))
-print(tree.inorder_print(tree.root, ""))
+
+tree2 = BinaryTree("F")
+tree2.root.left = Node("B")
+tree2.root.right = Node("G")
+tree2.root.left.left = Node("A")
+tree2.root.left.right = Node("D")
+tree2.root.left.right.left = Node("C")
+tree2.root.left.right.right = Node("E")
+tree2.root.right.right = Node("I")
+tree2.root.right.right.left = Node("H")
+
+print(tree2.pre_order_print(tree2.root, ""))
+print(tree2.inorder_print(tree2.root, ""))
+print(tree2.post_order_print(tree2.root, ""))
